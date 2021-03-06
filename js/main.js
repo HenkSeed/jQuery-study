4edb8de554e370ca4f0d8b55bb34a0ac3ff03ed5
HenkSeed <55748514+HenkSeed@users.noreply.github.com>
2021-03-06 23:49:37 +0700

    change-win

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
 	});
 
 	$("#change-button").click(function () {
-		$("#change-block").toggleClass("second-block-img");
+		alert("Что ты наделал !!!");
+		$("#first-block").toggleClass("first-block-img second-block-img");
+		$("#second-block").toggleClass("first-block-img second-block-img");
 	});
 });
