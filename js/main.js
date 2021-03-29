bfa41ed89a82d4ad7044f766fa7ff0252971aaad
HenkSeed <55748514+HenkSeed@users.noreply.github.com>
2021-03-29 13:46:30 +0700

    sweetAlert

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 $(document).ready(function () {
-	$("#first-button").click(function () {
-		$("#first-block").toggleClass("first-block-img");
+	$('#first-button').click(function () {
+		$('#first-block').toggleClass('first-block-img');
 	});
 
-	$("#second-button").click(function () {
-		$("#second-block").toggleClass("second-block-img");
+	$('#second-button').click(function () {
+		$('#second-block').toggleClass('second-block-img');
 	});
 
-	$("#change-button").click(function () {
-		alert("Что ты наделал !!!");
-		$("#first-block").toggleClass("first-block-img second-block-img");
-		$("#second-block").toggleClass("first-block-img second-block-img");
+	$('#change-button').click(function () {
+		// alert("Что ты наделал !!!");
+		swal({
+			title: 'ТРЕВОГА !!!',
+			text: 'Что ты наделал !?',
+			icon: 'warning',
+			buttons: ['А может не нажимать???', 'ЖМИ УЖЕ...'],
+		}).then((pressed) => {
+			if (pressed) {
+				swal('Замену сохраняю');
+			} else {
+				swal('', 'Ага, испугался ! Ладно, отменяю )', { button: 'ВОЗВРАТ' });
+				$('#first-block').toggleClass('first-block-img second-block-img');
+				$('#second-block').toggleClass('first-block-img second-block-img');
+			}
+		});
+		$('#first-block').toggleClass('first-block-img second-block-img');
+		$('#second-block').toggleClass('first-block-img second-block-img');
 	});
 });
